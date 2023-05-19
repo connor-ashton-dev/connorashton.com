a91b3e94faa00516f3a8c58eb9e8c942b09884f3
Connor Ashton <cjconnorashton@gmail.com>
2023-05-19 14:48:19 -0600

    changed pathman

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -14,5 +14,5 @@ export async function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: `/resume/:pathname*`,
+  matcher: `/application/:pathname*`,
 };
