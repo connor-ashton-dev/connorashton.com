bd50e19318f970f00060ad2aba80ace341bfb9f1
Connor Ashton <cjconnorashton@gmail.com>
2023-05-19 14:46:48 -0600

    added better texts

--- client/src/app/api/hello/route.ts
+++ client/src/app/api/hello/route.ts
@@ -3,12 +3,16 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 export async function GET(request: Request) {
-  console.log('ouch');
-  client.messages
-    .create({
-      body: 'Someone looked at your resume',
-      from: process.env.TWILIO_FROM_NUMBER,
-      to: process.env.TWILIO_TO_NUMBER,
-    })
-    .then((message: any) => console.log(message.sid));
+  const { searchParams } = new URL(request.url);
+  const who = searchParams.get('requester');
+  if (who) {
+    console.log(who.slice(8));
+    client.messages
+      .create({
+        body: `${who} looked at your resume`,
+        from: process.env.TWILIO_FROM_NUMBER,
+        to: process.env.TWILIO_TO_NUMBER,
+      })
+      .then((message: any) => console.log(message.sid));
+  }
 }
--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -3,15 +3,16 @@ import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
   const LINK =
     process.env.NODE_ENV == 'development'
       ? 'http://localhost:3000'
       : 'https://www.connor-ashton.com';
-  await fetch(`${LINK}/api/hello`);
+  await fetch(`${LINK}/api/hello?requester=${pathname}`);
   return NextResponse.redirect(new URL('/', request.url));
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: `/resume`,
+  matcher: `/resume/:pathname*`,
 };
